3e4b36dc848c94b9d8cb29fcd441cd7b11347212
Rob C <caleror1083@gmail.com>
2022-08-11 19:08:01 -0400

    No changes

--- jQuery/Practice/_001_DOM/assets/javascript/script.js
+++
@@ -1,9 +0,0 @@
-// Variable declarations and initializations
-var targetDiv = document.getElementById("empty-div");  // vGrab the "empty-div" div id from the HTML page using the getElementById selector.
-var newDiv = document.createElement("div");  // Create a new div called "newDiv" using the createElement selector
-
-// Process
-targetDiv.textContent = "Hello friends!";  // Using property textContent to change the content to "Hello friends!"
-newDiv.textContent = "A pleasure to meet you!";
-targetDiv.appendChild(newDiv);  // Using the appendChild method to combine the two divs together on the page.
-newDiv.setAttribute("class", "fancy");    // We then apply CSS to our newDiv.
