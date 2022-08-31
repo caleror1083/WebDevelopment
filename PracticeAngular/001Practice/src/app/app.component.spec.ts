ba607c7ec59b5fe4c980910e299d88331245c27f
Rob C <caleror1083@gmail.com>
2022-08-31 05:54:57 -0400

    No changes

--- PracticeAngular/001Practice/src/app/app.component.spec.ts
+++ PracticeAngular/001Practice/src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title '001Practice'`, () => {
+  it(`should have as title 'Robs practice application'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('001Practice');
+    expect(app.title).toEqual('Robs practice application');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('001Practice app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('Robs practice application is running!');
   });
 });
