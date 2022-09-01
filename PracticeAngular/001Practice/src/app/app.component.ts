2526da3d622bdacaa5a5466d27e2fc78b4958938
Rob C <caleror1083@gmail.com>
2022-09-01 06:16:10 -0400

    No changes

--- PracticeAngular/001Practice/src/app/app.component.ts
+++ PracticeAngular/001Practice/src/app/app.component.ts
@@ -1,9 +1,12 @@
+// imports
 import { Component } from '@angular/core';
 
-@Component({
-    selector: 'app-root',
-    template: '<div><h1>{{pageTitle}}</h1><div>My First Component</div></div>'
-})
-export class AppComponent {
-    pageTitle: string = 'Acme Product Management';
-}
+// decorator functions (metadata and templates)
+@Component( {selector: 'app-root', template: '<div><h1>{{pageTitle}}</h1><div>My First Component</div></div>' })
+
+// classes
+export class AppComponent
+    {
+        // properties
+        pageTitle: string = 'Acme Product Management';
+    }
