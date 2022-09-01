eeb312ad329546995fc5199554fa4db98c1d7ec4
Rob C <caleror1083@gmail.com>
2022-08-31 20:51:08 -0400

    No changes

--- PracticeAngular/001Practice/src/app/app.component.ts
+++ PracticeAngular/001Practice/src/app/app.component.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    template: '<div><h1>{{pageTitle}}</h1><div>My First Component</div></div>'
 })
 export class AppComponent {
-  title = 'Robs practice application';
+    pageTitle: string = 'Acme Product Management';
 }
