0163343290c803b0ab2599dfcc3b682cf647be3f
Rob C <caleror1083@gmail.com>
2022-09-03 00:25:54 -0400

    No changes

--- PracticeAngular/001Practice/src/app/app.component.ts
+++ PracticeAngular/001Practice/src/app/app.component.ts
@@ -1,13 +1,13 @@
-// imports
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
-// decorator functions (metadata and templates)
 @Component({
-  selector: 'app-root',
-  template: `<h1>Hello {{name}}</h1>`
+     selector: 'app-root',
+     templateUrl: 'app.component.html'
+     // template: `<h1>Hello {{ name }}</h1>`
 })
 
-// classes
-export class AppComponent { 
-  name: string = 'Angular 2!';  // string property called name with a value
+// the root component
+// export keyword allows this class to be imported and used by other components
+export class AppComponent {
+    name : string = "Angular";  // string property called name with a value
 }
