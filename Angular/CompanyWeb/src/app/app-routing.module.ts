40f84496f96706c6068681e76cadb62e5b12c8da
Rob C <caleror1083@gmail.com>
2022-09-11 20:54:43 -0400

    No changes

--- Angular/CompanyWeb/src/app/app-routing.module.ts
+++ Angular/CompanyWeb/src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { DepartmentsComponent } from './departments/departments.component';
+import { EmployeesComponent } from './employees/employees.component';
+
+const routes: Routes = [
+  { path: 'employees', component: EmployeesComponent },
+  { path: 'departments', component: DepartmentsComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
+
 export class AppRoutingModule { }
