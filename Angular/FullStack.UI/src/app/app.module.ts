667264597b225bc2be49a4be677ed884ac7341b5
Rob C <caleror1083@gmail.com>
2023-01-16 13:05:39 -0500

    Employees-list component created

--- Angular/FullStack.UI/src/app/app.module.ts
+++ Angular/FullStack.UI/src/app/app.module.ts
@@ -3,9 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EmployeesListComponent } from './components/employees/employees-list/employees-list.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, EmployeesListComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent]
