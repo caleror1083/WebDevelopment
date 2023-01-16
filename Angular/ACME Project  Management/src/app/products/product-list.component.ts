09af526a362a8e7cd6032a4727f6327a10db8536
Rob C <caleror1083@gmail.com>
2023-01-16 11:33:19 -0500

    updated project to Angular 15

--- Angular/ACME Project  Management/src/app/products/product-list.component.ts
+++ Angular/ACME Project  Management/src/app/products/product-list.component.ts
@@ -1,5 +1,6 @@
+import { isNgTemplate } from "@angular/compiler";
 import { Component, OnDestroy, OnInit } from "@angular/core";
-import { Subscription } from "rxjs";
+import { Subscription, of, from, map, tap, take } from "rxjs";
 import { IProduct } from "./product";
 import { ProductService } from "./product.service";
 
@@ -47,6 +48,31 @@ export class ProductListComponent implements OnInit, OnDestroy {
       },
       error: err => this.errorMessage = err
     });
+
+    of(2, 4, 6, 8)
+      .subscribe(
+        item => console.log(item)
+      );
+
+    console.log('')
+
+    from([10, 20, 30, 40])
+      .pipe(
+        tap(item => console.log(`emitted item... ${item}`)),
+        map(item => item * 2),
+        map(item => item - 10),
+        map(item => {
+          if (item === 0) {
+            throw new Error('zero detected');
+          }
+          return item;
+        }),
+        take(3))
+      .subscribe({
+        next: item => console.log(`resulting item... ${item}`),
+        error: err => console.error(`error occured${err}`),
+        complete: () => console.log(`complete`),
+      });
   }
 
   ngOnDestroy(): void {
