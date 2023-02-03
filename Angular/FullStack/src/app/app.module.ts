c2e8e82f255c0d631155774eb999a3d1dcdee0df
Rob C <caleror1083@gmail.com>
2023-02-02 21:05:54 -0500

    initial commit

--- Angular/FullStack/src/app/app.module.ts
+++ Angular/FullStack/src/app/app.module.ts
@@ -3,13 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { EmployeesListComponent } from './components/employees/employees-list/employees-list.component';
 
 @NgModule({
-  declarations: [AppComponent, EmployeesListComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
-
 export class AppModule { }
